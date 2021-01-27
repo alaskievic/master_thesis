8d278741d87479ff155c9556f85f8e89db3f64bb
Andrei Arminio Laskievic <alaskievic@gmail.com>
2021-01-27 11:56:31 -0300

    inor changes

--- RScripts/14_mun_gdps.R
+++ RScripts/14_mun_gdps.R
@@ -12,7 +12,6 @@ municip_pib_2002 <- read_excel("C:/Users/Andrei/Desktop/Dissertation/Analysis/Da
 
 municip_pib_2002 %<>% rename(cod = "...1", municip = "...2") %>%
   dplyr::select(c("cod", "municip", "1999", "2000", "2001")) %>%
-  filter(cod > 1) %>%
   slice(-(n())) %>%
   mutate(cod = as.integer(cod)) %>%
   arrange(cod)
@@ -23,7 +22,6 @@ municip_pib_agro_2002 <- read_excel("C:/Users/Andrei/Desktop/Dissertation/Analys
 
 municip_pib_agro_2002 %<>% rename(cod = "...1", municip = "...2") %>%
   dplyr::select(c("cod", "municip",  "1999", "2000", "2001")) %>%
-  filter(cod > 1) %>%
   slice(-(n())) %>%
   mutate(cod = as.integer(cod)) %>%
   arrange(cod)
@@ -35,7 +33,6 @@ municip_pib_indust_2002 <- read_excel("C:/Users/Andrei/Desktop/Dissertation/Anal
 
 municip_pib_indust_2002 %<>% rename(cod = "...1", municip = "...2") %>%
   dplyr::select(c("cod", "municip", "1999", "2000", "2001")) %>%
-  filter(cod > 1) %>%
   slice(-(n())) %>%
   mutate(cod = as.integer(cod)) %>%
   arrange(cod)
@@ -47,7 +44,6 @@ municip_pib_serv_2002  <- read_excel("C:/Users/Andrei/Desktop/Dissertation/Analy
 
 municip_pib_serv_2002 %<>% rename(cod = "...1", municip = "...2") %>%
   dplyr::select(c("cod", "municip",  "1999", "2000", "2001")) %>%
-  filter(cod > 1) %>%
   slice(-(n())) %>%
   mutate(cod = as.integer(cod)) %>%
   arrange(cod)
@@ -60,7 +56,6 @@ municip_pib_2010 <- read_excel("C:/Users/Andrei/Desktop/Dissertation/Analysis/Da
                               skip = 3, sheet = 1, col_names = TRUE, na = c("NA","N/A","", "...", "-", "..", "X"))
 
 municip_pib_2010  %<>% rename(cod = "...1", municip = "...2") %>%
-  filter(cod > 1) %>%
   slice(-(n())) %>%
   mutate(cod = as.integer(cod)) %>%
   arrange(cod)
@@ -70,7 +65,6 @@ municip_pib_agro_2010 <- read_excel("C:/Users/Andrei/Desktop/Dissertation/Analys
                                  skip = 3, sheet = 2, col_names = TRUE, na = c("NA","N/A","", "...", "-", "..", "X"))
 
 municip_pib_agro_2010 %<>% rename(cod = "...1", municip = "...2") %>%
-  filter(cod > 1) %>%
   slice(-(n())) %>%
   mutate(cod = as.integer(cod)) %>%
   arrange(cod)
@@ -80,7 +74,6 @@ municip_pib_indust_2010 <- read_excel("C:/Users/Andrei/Desktop/Dissertation/Anal
                                    skip = 3, sheet = 1, col_names = TRUE, na = c("NA","N/A","", "...", "-", "..", "X"))
 
 municip_pib_indust_2010 %<>% rename(cod = "...1", municip = "...2") %>%
-  filter(cod > 1) %>%
   slice(-(n())) %>%
   mutate(cod = as.integer(cod)) %>%
   arrange(cod)
@@ -91,7 +84,6 @@ municip_pib_serv1_2010 <- read_excel("C:/Users/Andrei/Desktop/Dissertation/Analy
 
 
 municip_pib_serv1_2010 %<>% rename(cod = "...1", municip = "...2") %>%
-  filter(cod > 1) %>%
   slice(-(n())) %>%
   mutate(cod = as.integer(cod)) %>%
   pivot_longer(-c("cod", "municip"), names_to = "year", values_to = "pib_serv") %>%
@@ -102,7 +94,6 @@ municip_pib_serv2_2010 <- read_excel("C:/Users/Andrei/Desktop/Dissertation/Analy
            skip = 3, sheet = 1, col_names = TRUE, na = c("NA","N/A","", "...", "-", "..", "X"))
 
 municip_pib_serv2_2010 %<>% rename(cod = "...1", municip = "...2") %>%
-  filter(cod > 1) %>%
   slice(-(n())) %>%
   mutate(cod = as.integer(cod)) %>%
   pivot_longer(-c("cod", "municip"), names_to = "year", values_to = "pib_serv") %>%
@@ -170,10 +161,62 @@ municip_pib_real <- mutate_all(municip_pib[4:7], deflate) %>%
 municip_pib_real <- municip_pib_real %>% mutate(cod = as.integer(cod)) %>%
   mutate(year = as.integer(year))
 
-municip_pib_real <- municip_pib_real %>% filter(year >= 2000 & year <= 2015)
 
-save(municip_pib_real,
+
+municip_pib_final <- municip_pib_real %>% filter(year >= 2000 & year <= 2015) %>%
+  filter(cod>1)
+
+save(municip_pib_final,
      file = "C:/Users/Andrei/Desktop/Dissertation/Analysis/master_thesis/Final Datasets/municip_pib_real.Rdata")
+
+
+
+######### 2. GDP Shares Graph  #################################################
+
+
+gdp_shares <- municip_pib_real %>% filter (cod==1) %>% 
+  mutate(gdpagro_sh = pib_agro/pib_tot) %>%
+  mutate(gdpindust_sh = pib_indust/pib_tot) %>%
+  mutate(gdpserv_sh = pib_serv/pib_tot) %>%
+  dplyr::select(cod, municip, year, gdpagro_sh, gdpindust_sh, gdpserv_sh) %>%
+  pivot_longer(-c("cod", "municip", "year"), names_to = "Sector", values_to = "shares")
+
+
+# Defining Palette
+c25 <- c(
+  "dodgerblue2", 
+  "#E31A1C", # red
+  "green4",
+  "#6A3D9A", # purple
+  "#FF7F00", # orange
+  "black", "gold1",
+  "skyblue2", "#FB9A99", # lt pink
+  "palegreen2",
+  "#CAB2D6", # lt purple
+  "#FDBF6F", # lt orange
+  "gray70", "khaki2",
+  "maroon", "orchid1", "deeppink1", "blue1", "steelblue4",
+  "darkturquoise", "green1", "yellow4", "yellow3",
+  "darkorange4", "brown"
+)
+
+
+sh_graph <- ggplot(gdp_shares, aes(x=year)) +
+  geom_line(data = gdp_shares, aes(y=shares, color = Sector, group = Sector), lwd = 1)+
+  scale_color_manual(values=c25, 
+                     labels = c("Agriculture", "Manufacturing", "Services"))+
+  geom_point(data = gdp_shares, aes(x=year, y=shares, color = Sector), size=2)+
+  labs(x = "Year", y = "Employment Shares") +
+  theme_bw(base_size = 13) +
+  theme(legend.text=element_text(size=13), legend.position = c(0.85, 0.6), 
+        legend.box.background = element_blank(), legend.title = element_blank())
+
+sh_graph
+ggsave(filename = "empshares.png", plot = sh_graph, 
+       path = "C:/Users/Andrei/Desktop/Dissertation/Analysis/master_thesis/Figures")
+
+
+
 
 
 
